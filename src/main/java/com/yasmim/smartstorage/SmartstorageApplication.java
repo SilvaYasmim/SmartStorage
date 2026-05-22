b59cad9f7d9a932f4202b96f159d47ccbde74a0e
SilvaYasmim <yasmim.design@hotmail.com>
2026-05-22 14:38:08 +0100

    feat: setup inicial do projeto Spring Boot

---
+++ src/main/java/com/yasmim/smartstorage/SmartstorageApplication.java
@@ -0,0 +1,13 @@
+package com.yasmim.smartstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasmim/smartstorage/SmartstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasmim.smartstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
